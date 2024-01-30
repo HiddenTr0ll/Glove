02cc2098af3e057740a0d3104282a0484558fe04
HiddenTroll <jonas@stoetzelweb.de>
2024-01-30 13:51:47 +0100

    fix MacOS-fix

--- settings.py
+++ settings.py
@@ -165,7 +165,7 @@ def create_shader_program(vertex_filepath: str, fragment_filepath: str) -> int:
     vertex_module = create_shader_module(vertex_filepath, GL_VERTEX_SHADER)
     fragment_module = create_shader_module(fragment_filepath, GL_FRAGMENT_SHADER)
 
-    if platform.system() == "Darwin":
+    if sys.platform.startswith('darwin'):
         shader = compileProgram(vertex_module, fragment_module, validate=False)
     else:
         shader = compileProgram(vertex_module, fragment_module)
