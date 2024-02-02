454529cf0782292e9fde9ec3bd0415a3ceba050c
HiddenTroll <jonas@stoetzelweb.de>
2024-02-02 14:22:28 +0100

    added better finger approximation. still not perfect

--- finger.py
+++ finger.py
@@ -26,15 +26,15 @@ class Finger():
 
     def updateTipRotation(self, rotationTip, rotationPalm):
         self.limbs[2].rotation = rotationTip
-        self.calculateSubRotations(rotationTip, rotationPalm)
+        self.calculateSubRotations2(rotationTip, rotationPalm)
 
     def calculateSubRotations(self, rotationTip, rotationPalm):
         tipQuat = pyrr.Quaternion.from_matrix(rotationTip)
         palmQuat = pyrr.Quaternion.from_matrix(rotationPalm)
         identityQuat = pyrr.Quaternion(pyrr.Matrix44.identity())
         dist = palmQuat.inverse * tipQuat  # rotation of tip without rotation of palm
-        if (dist.w < 0):
-            dist = - dist
+        # if (dist.w < 0):
+        #    dist = - dist
         finger1 = palmQuat * identityQuat.slerp(dist, 0.333)
         finger2 = palmQuat * identityQuat.slerp(dist, 0.666)
 
@@ -44,6 +44,48 @@ class Finger():
         # TODO seperate swing and twist?!
         # TODO implement natural divisions https://scielo.conicyt.cl/pdf/ijmorphol/v28n3/art15.pdf
         pass
+
+    def calculateSubRotations2(self, rotationTip, rotationPalm):
+        tipQuat = pyrr.Quaternion.from_matrix(rotationTip)
+        palmQuat = pyrr.Quaternion.from_matrix(rotationPalm)
+        identityQuat = pyrr.Quaternion(pyrr.Matrix44.identity())
+        dist = tipQuat*palmQuat.inverse   # rotation of tip without rotation of palm
+
+        swing, twist = self.seperateSwingTwist(dist)
+
+        if (swing.w < 0):
+            swing = - swing
+
+        finger1 = identityQuat.slerp(swing, 0.33333) * twist * palmQuat
+        finger2 = identityQuat.slerp(swing, 0.66666) * twist * palmQuat
+
+        fingerAdjust1 = pyrr.Quaternion.from_x_rotation(np.deg2rad(0.333*-360))
+        fingerAdjust2 = pyrr.Quaternion.from_x_rotation(np.deg2rad(0.666*-360))
+        # TODO Stop flipping by 180 deg...
+        if rotationTip != pyrr.Matrix44.identity():
+            # print(swing)
+            pass
+
+        # if swing.x > 0:
+        #    if (swing.w < 0):
+        #        finger1 = fingerAdjust1 * finger1
+        #        finger2 = fingerAdjust2*finger2
+        #        print("<0.7")
+
+        self.limbs[0].rotation = pyrr.matrix44.create_from_quaternion(finger1)
+        self.limbs[1].rotation = pyrr.matrix44.create_from_quaternion(finger2)
+
+    def seperateSwingTwist(self, toSeperate: pyrr.Quaternion):
+        direction = np.array([0, 0, 1])
+
+        # singularity: rotation by 180 degree
+
+        swing = pyrr.Quaternion(np.array([toSeperate.x, 0, 0, toSeperate.w]))
+        swing = swing.normalised
+
+        twist = swing.inverse * toSeperate
+
+        return swing, twist
 
     def updatePosition(self, palmTip, palmRotation, offsetX, offsetZ):
         # calculate fingerBot position
